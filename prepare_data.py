38e663fa4fadc2152436c4476956f48d1c4868a3
Nicolas Thiers <thiers0510@gmail.com>
2017-07-30 22:34:54 -0400

    update file, add programmableFilter for compute RMS fluctuations

--- prepare_data.py
+++ prepare_data.py
@@ -3,13 +3,13 @@ from paraview.simple import *
 #### disable automatic camera reset on 'Show'
 
 # create a new 'VisItNek5000Reader'
-avg_data = VisItNek5000Reader(FileName= 'Data/.avghigh_ray.nek5000')
+avg_data = VisItNek5000Reader(FileName= 'Data/avghigh_ray.nek5000')
 avg_data.Meshes = ['mesh']
 avg_data.Materials = []
 avg_data.CellArrays = []
 avg_data.PointArrays = ['pressure', 'temperature', 'x_velocity', 'y_velocity', 'z_velocity']
 
-rms_data = VisItNek5000Reader(FileName= 'Data/.rmshigh_ray.nek5000')
+rms_data = VisItNek5000Reader(FileName= 'Data/rmshigh_ray.nek5000')
 rms_data.Meshes = ['mesh']
 rms_data.Materials = []
 rms_data.CellArrays = []
@@ -30,27 +30,27 @@ rms_temporalStatistics.ComputeStandardDeviation = 0
 
 # create a new 'Programmable Filter'
 programmableFilter1 = ProgrammableFilter(Input=[avg_data, rms_data])
-programmableFilter1.Script ='\n
-meanU = inputs[0].PointData["x_velocity"]\n
-meanUU = inputs[1].PointData["x_velocity"]\n
-meanV = inputs[0].PointData["y_velocity"]\n
-meanVV = inputs[1].PointData["y_velocity"]\n
-meanW = inputs[0].PointData["z_velocity"]\n
-meanWW = inputs[1].PointData["z_velocity"]\n
-meanP = inputs[0].PointData["pressure"]\n
-meanPP = inputs[1].PointData["pressure"]\n
-meanT = inputs[0].PointData["temperature"]\n
-meanTT = inputs[1].PointData["temperature"]\n
-output.PointData.append(meanU,"<u>")\n
-output.PointData.append(meanV,"<v>")\n
-output.PointData.append(meanW,"<w>")\n
-output.PointData.append(meanP,"<P>")\n
-output.PointData.append(meanP,"<T>")\n
-output.PointData.append(meanVV-meanV*meanV,"<v\'v\'>")\n
-output.PointData.append(meanUU-meanU*meanU,"<u\'u\'>")\n
-output.PointData.append(meanWW-meanW*meanW,"<w\'w\'>")\n
-output.PointData.append(meanPP-meanP*meanP,"<P\'>")\n
-output.PointData.append(meanTT-meanT*meanT,"<T\'>")\n'
+programmableFilter1.Script ='\
+	meanU = inputs[0].PointData["x_velocity"]\n\
+	meanUU = inputs[1].PointData["x_velocity"]\n\
+	meanV = inputs[0].PointData["y_velocity"]\n\
+	meanVV = inputs[1].PointData["y_velocity"]\n\
+	meanW = inputs[0].PointData["z_velocity"]\n\
+	meanWW = inputs[1].PointData["z_velocity"]\n\
+	meanP = inputs[0].PointData["pressure"]\n\
+	meanPP = inputs[1].PointData["pressure"]\n\
+	meanT = inputs[0].PointData["temperature"]\n\
+	meanTT = inputs[1].PointData["temperature"]\n\
+	output.PointData.append(meanU,"<u>")\n\
+	output.PointData.append(meanV,"<v>")\n\
+	output.PointData.append(meanW,"<w>")\n\
+	output.PointData.append(meanP,"<P>")\n\
+	output.PointData.append(meanP,"<T>")\n\
+	output.PointData.append(meanVV-meanV*meanV,"<v\'v\'>")\n\
+	output.PointData.append(meanUU-meanU*meanU,"<u\'u\'>")\n\
+	output.PointData.append(meanWW-meanW*meanW,"<w\'w\'>")\n\
+	output.PointData.append(meanPP-meanP*meanP,"<P\'>")\n\
+	output.PointData.append(meanTT-meanT*meanT,"<T\'>")\n'
 
 # save data
 #SaveData('Data/avg_data.csv', proxy=avg_temporalStatistics, Precision=5,
